52a8ca24e296029a6efbc89411c54cc52e3daa5c
chayleaf <chayleaf-git@pavluk.org>
2023-06-03 15:32:24 +0700

    qdl: unstable-2021-05-06 -> unstable-2023-04-11

--- pkgs/tools/misc/qdl/default.nix
+++ pkgs/tools/misc/qdl/default.nix
@@ -1,16 +1,17 @@
-{ lib, stdenv, fetchFromGitHub, libxml2, systemd }:
+{ lib, stdenv, fetchFromGitHub, libxml2, systemd, pkg-config }:
 
 stdenv.mkDerivation {
   pname   = "qdl";
-  version = "unstable-2021-05-06";
+  version = "unstable-2023-04-11";
 
   src = fetchFromGitHub {
-    owner  = "andersson";
-    repo   = "qdl";
-    rev    = "2021b303a81ca1bcf21b7f1f23674b5c8747646f";
-    sha256 = "0akrdca4jjdkfdya36vy1y5vzimrc4pp5jm24rmlw8hbqxvj72ri";
+    owner = "andersson";
+    repo  = "qdl";
+    rev   = "3b22df2bc7de02d867334af3a7aa8606db4f8cdd";
+    hash  = "sha256-2sL9HX73APTn9nQOx1Efdkz9F4bNASPMVFMx6YOqxyc=";
   };
 
+  nativeBuildInputs = [ pkg-config ];
   buildInputs = [ systemd libxml2 ];
 
   installPhase = ''
